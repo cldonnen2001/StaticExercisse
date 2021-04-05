449cea1636a39746217ffd9714bccd3f1a5ec959
Christine Godfrey <cldonnen2001@yahoo.com>
2021-04-05 17:58:53 -0500

    exercises finished

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(73);  // w/out readline
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(12);  // w/out readline
+
+            Console.WriteLine($"Fahrenheit to Celcius is {celsius}");
+            Console.WriteLine($"Celsius to Fahrenheit is {fahrenheit}");
+
+            //  John way
+
+            // Console.WriteLine(TempConverter.FahrenheitToCelsius(32));
+            // Console.WriteLine(empConverter.CelsiusToFahrenheit(0));
+            // Console.WriteLine(empConverter.CelsiusToFahrenheit(100));
+            //  Console.WriteLine(TempConverter.FahrenheitToCelsius(212));
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+       public static double FahrenheitToCelsius(double fahrenheit)
+       {
+          var result = (fahrenheit-32) / 1.8;
+            return result;
+       }
+       public static double CelsiusToFahrenheit(double celsius)
+       {
+            var result = (celsius * 9) / 5 + 32;
+            return result;
+        }
+
+        //John way
+
+        // public static double F = 0;
+        // public static double C = 0;
+        // public static double FahrenheitToCelsius(double fTemp) 
+        // {
+        // C = (fTemp - 32) / 1.8000;
+        // return C;
+        // }
+
+        // public static double CelsiusToFahrenheit(double cTemp)
+        // {
+        // F = (cTemp * (9/5)) + 32;;
+        // return F;
+        // }
+
+
+
+    }
+}
